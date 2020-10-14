714294d63560784610af75a80a5f358e146ff580
harshdulani <harshdulani@yahoo.in>
2020-10-14 12:36:08 +0530

    Unit now travels on a smoothed path

--- Assets/Scripts/UnitForPath.cs
+++ Assets/Scripts/UnitForPath.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class UnitForPath : MonoBehaviour
 {
+    const float minPathUpdateTime = 0.2f;
+    const float pathUpdateMoveThreshold = 0.5f;
+
     public Transform target;
-    public float speed = 20f;
+    public float speed = 20f, turnSpeed = 3f;
     public int turnRadius = 5;
 
     public bool drawPathInEditor = false;
@@ -14,7 +17,7 @@ public class UnitForPath : MonoBehaviour
 
     private void Start()
     {
-        PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+        StartCoroutine("UpdatePath");
     }
 
     public void OnPathFound(Vector3[] waypoints,  bool pathSuccess)
@@ -27,10 +30,52 @@ public class UnitForPath : MonoBehaviour
         }
     }
 
-    private IEnumerator FollowPath()
+    private IEnumerator UpdatePath()
     {
+        if (Time.timeSinceLevelLoad < 0.3f)
+            yield return new WaitForSeconds(0.3f);
+
+        PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+
+        var sqrMoveThreshold = pathUpdateMoveThreshold * pathUpdateMoveThreshold;
+        Vector3 targetPosOld = target.position;
+
         while (true)
         {
+            yield return new WaitForSeconds(minPathUpdateTime);
+            //only request a new path if the target has moved farther away
+            if((target.position - targetPosOld).sqrMagnitude > sqrMoveThreshold)
+                PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+        }
+    }
+
+    private IEnumerator FollowPath()
+    {
+        bool followingPath = true;
+        int pathIndex = 0;
+        transform.LookAt(path.lookPoints[0]);
+
+        while (followingPath)
+        {
+            Vector2 position = new Vector2(transform.position.x, transform.position.z);
+            while (path.turnBoundaries[pathIndex].HasCrossedLine(position))
+            {
+                if (pathIndex == path.finishLineIndex)
+                { 
+                    followingPath = false;
+                    break;
+                }
+                else
+                    pathIndex++;
+            }
+
+            if(followingPath)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(path.lookPoints[pathIndex] - transform.position);
+
+                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+                transform.Translate(Vector3.forward * Time.deltaTime * speed, Space.Self);
+            }
             yield return null;
         }
     }
